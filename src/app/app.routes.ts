29bc103680305890b2d05c6bcfe22f55ef7fedea
Radu-A <elcastravete@gmail.com>
2026-01-18 17:38:31 +0100

    chore: create component structure, install primeng and add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './features/product-list/product-list';
+import { ProductDetails } from './features/product-details/product-details';
+import { LoginView } from './features/login-view/login-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'articles',
+    redirectTo: '/products',
+  },
+  {
+    path: 'products',
+    component: ProductList,
+  },
+  {
+    path: 'details',
+    component: ProductDetails,
+  },
+  {
+    path: 'login',
+    component: LoginView,
+  },
+];
