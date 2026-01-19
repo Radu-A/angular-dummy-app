b042e1de0cddb4fb3d686fd4c7491dff8f383a20
Radu-A <elcastravete@gmail.com>
2026-01-19 19:16:47 +0100

    feat(footer): add basic footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatToolbarModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
-
-}
+export class Footer {}
