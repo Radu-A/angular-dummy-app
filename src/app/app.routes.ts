b53a806839bc27b699290dbf1243826994ff0fdf
Radu-A <elcastravete@gmail.com>
2026-01-19 17:47:18 +0100

    feat(main-layout): configure layout and adapt app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,40 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layout/main-layout/main-layout';
 import { ProductList } from './features/product-list/product-list';
 import { ProductDetails } from './features/product-details/product-details';
 import { LoginView } from './features/login-view/login-view';
 
 export const routes: Routes = [
   {
-    path: 'articles',
+    path: '',
     redirectTo: '/products',
-  },
-  {
-    path: 'products',
-    component: ProductList,
-  },
-  {
-    path: 'details',
-    component: ProductDetails,
+    pathMatch: 'full',
   },
   {
     path: 'login',
     component: LoginView,
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'products',
+        children: [
+          {
+            path: '',
+            component: ProductList,
+          },
+          {
+            path: 'details',
+            component: ProductDetails,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/products',
   },
 ];
